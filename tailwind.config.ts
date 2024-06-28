0c3fa61f66eb9449788609d07188c2d44f53f58c
Basir Khan <khanbasir5555@gmail.com>
2024-06-28 11:34:00 +0530

    navbar added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ module.exports = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
+        bgcust:"#02B3E4",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
