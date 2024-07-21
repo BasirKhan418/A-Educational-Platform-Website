5eaaf883450facb069a98c870ccc3e88e88d2f4e
yagyesh_bobde <yagyeshsrbobde@gmail.com>
2024-07-22 00:59:15 +0530

    updated enquiry page

--- src/utils/Enquiry2.tsx
+++ src/utils/Enquiry2.tsx
@@ -1,14 +1,24 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { ArrowLeft, MapPin, PhoneCall } from "lucide-react";
+import { ArrowLeft, Check, MapPin, PhoneCall } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useToast } from "@/components/ui/use-toast";
+
 
 const Enquiry2 = () => {
   const router = useRouter();
 
+  const [formSubmitted, setformSubmitted] = useState(false)
   const [formData, setFormData] = useState({
     studentName: "",
-    std: "",
+    std: "VII",
     schoolName: "",
     email: "",
     address: "",
@@ -26,7 +36,12 @@ const Enquiry2 = () => {
     demoClass: false,
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | string) => {
+    if(typeof e === "string"){
+      setFormData({ ...formData, std: e });
+      setErrors({ ...errors, std: false });
+      return;
+    }
     setFormData({ ...formData, [e.target.id]: e.target.value });
     setErrors({ ...errors, [e.target.id]: false });
   };
@@ -36,6 +51,7 @@ const Enquiry2 = () => {
     setErrors({ ...errors, demoClass: false });
   };
 
+  const { toast } = useToast();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const newErrors = {
@@ -51,7 +67,14 @@ const Enquiry2 = () => {
 
     const hasErrors = Object.values(newErrors).some((error) => error);
     if (!hasErrors) {
-      router.push("/");
+      toast({
+        title: "Thank your for showing interest!",
+        description: "We will reach out to you soon.",
+      })
+      setformSubmitted(true);
+      setTimeout(() => {
+        router.push("/");
+      }, 1500);
     }
   };
   const handleBackClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -87,8 +110,7 @@ const Enquiry2 = () => {
       </div>
       <div className="z-50 flex min-h-screen items-center justify-center ">
         <div className="w-full max-w-6xl rounded-lg shadow-xl md:absolute md:top-52 lg:absolute lg:top-52">
-          <div className="grid bg-white grid-cols-1 gap-6 md:grid-cols-2">
-
+          <div className="grid grid-cols-1 gap-6 bg-white md:grid-cols-2">
             {/* LEFT CONTACT SIDE */}
             <div className="flex flex-col justify-between rounded-lg bg-gradient-to-b from-[#02B3E4] to-[#0790B6] p-6 text-white md:p-16">
               <div className="flex items-center gap-4">
@@ -131,183 +153,203 @@ const Enquiry2 = () => {
 
             {/* RIGHT CONTACT SIDE */}
             <div className="poppins-medium rounded-lg bg-white p-6">
-              <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                  <label
-                    className="mb-2 block font-bold text-gray-700"
-                    htmlFor="student-name"
-                  >
-                    Student Name
-                  </label>
-                  <input
-                    type="text"
-                    id="studentName"
-                    value={formData.studentName}
-                    onChange={handleChange}
-                    placeholder="Enter student name"
-                    className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.studentName ? "border-red-500" : ""}`}
-                  />
-                  {errors.studentName && (
-                    <p className="text-xs italic text-red-500">
-                      Please enter the student&apos;s name.
-                    </p>
-                  )}
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="mb-2 block font-bold text-gray-700"
-                    htmlFor="std"
-                  >
-                    STD
-                  </label>
-                  <input
-                    type="text"
-                    id="std"
-                    value={formData.std}
-                    onChange={handleChange}
-                    className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.std ? "border-red-500" : ""}`}
-                  />
-                  {errors.std && (
-                    <p className="text-xs italic text-red-500">
-                      Select a standard
-                    </p>
-                  )}
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="mb-2 block font-bold text-gray-700"
-                    htmlFor="school-name"
-                  >
-                    School Name
-                  </label>
-                  <input
-                    type="text"
-                    id="schoolName"
-                    value={formData.schoolName}
-                    onChange={handleChange}
-                    className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.schoolName ? "border-red-500" : ""}`}
-                  />
-                  {errors.schoolName && (
-                    <p className="text-xs italic text-red-500">
-                      Please enter the school name.
-                    </p>
-                  )}
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="mb-2 block font-bold text-gray-700"
-                    htmlFor="email-address"
-                  >
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.email ? "border-red-500" : ""}`}
-                    placeholder="e.g. mary@example.com"
-                  />
-                  {errors.email && (
-                    <p className="text-xs italic text-red-500">
-                      Please enter a valid email address.
-                    </p>
-                  )}
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="mb-2 block font-bold text-gray-700"
-                    htmlFor="address"
-                  >
-                    Address
-                  </label>
-                  <input
-                    type="text"
-                    id="address"
-                    value={formData.address}
-                    onChange={handleChange}
-                    className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.address ? "border-red-500" : ""}`}
-                  />
-                  {errors.address && (
-                    <p className="text-xs italic text-red-500">
-                      Please enter the address.
-                    </p>
-                  )}
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="mb-2 block font-bold text-gray-700"
-                    htmlFor="mobile-number"
-                  >
-                    Mobile Number
-                  </label>
-                  <input
-                    type="text"
-                    id="mobileNumber"
-                    value={formData.mobileNumber}
-                    onChange={handleChange}
-                    className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.mobileNumber ? "border-red-500" : ""}`}
-                    placeholder="+91xxxxxxxxxx"
-                  />
-                  {errors.mobileNumber && (
-                    <p className="text-xs italic text-red-500">
-                      Please enter a valid mobile number.
-                    </p>
-                  )}
-                </div>
-                <div className="mb-4">
-                  <label className="mb-2 block font-bold text-gray-700">
-                    Interested in Demo Class
-                  </label>
-                  <div className="flex items-center">
-                    <input
-                      type="radio"
-                      id="yes"
-                      name="demoClass"
-                      value="yes"
-                      checked={formData.demoClass === "yes"}
-                      onChange={handleRadioChange}
-                      className="mr-2 leading-tight"
-                    />
-                    <label className="text-gray-700" htmlFor="yes">
-                      Yes
+              {!formSubmitted ? (
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-4">
+                    <label
+                      className="mb-2 block font-bold text-gray-700"
+                      htmlFor="student-name"
+                    >
+                      Student Name
                     </label>
                     <input
-                      type="radio"
-                      id="no"
-                      name="demoClass"
-                      value="no"
-                      checked={formData.demoClass === "no"}
-                      onChange={handleRadioChange}
-                      className="ml-4 mr-2 leading-tight"
+                      type="text"
+                      id="studentName"
+                      value={formData.studentName}
+                      onChange={handleChange}
+                      placeholder="Enter student name"
+                      className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.studentName ? "border-red-500" : ""}`}
                     />
-                    <label className="text-gray-700" htmlFor="no">
-                      No
+                    {errors.studentName && (
+                      <p className="text-xs italic text-red-500">
+                        Please enter the student&apos;s name.
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="mb-2 block font-bold text-gray-700"
+                      htmlFor="std"
+                    >
+                      STD
+                    </label>
+                    <Select name="std" value={formData.std} onValueChange={handleChange}>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a standard" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value={"VII"}>VII</SelectItem>
+                        <SelectItem value="VIII">VIII</SelectItem>
+                        <SelectItem value="IX">IX</SelectItem>
+                        <SelectItem value="X">X</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    {/* <input type="text" id="std" value={formData.std} /> */}
+                    {errors.std && (
+                      <p className="text-xs italic text-red-500">
+                        Select a standard
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="mb-2 block font-bold text-gray-700"
+                      htmlFor="school-name"
+                    >
+                      School Name
+                    </label>
+                    <input
+                      type="text"
+                      id="schoolName"
+                      value={formData.schoolName}
+                      onChange={handleChange}
+                      className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.schoolName ? "border-red-500" : ""}`}
+                    />
+                    {errors.schoolName && (
+                      <p className="text-xs italic text-red-500">
+                        Please enter the school name.
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="mb-2 block font-bold text-gray-700"
+                      htmlFor="email-address"
+                    >
+                      Email Address
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.email ? "border-red-500" : ""}`}
+                      placeholder="e.g. mary@example.com"
+                    />
+                    {errors.email && (
+                      <p className="text-xs italic text-red-500">
+                        Please enter a valid email address.
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="mb-2 block font-bold text-gray-700"
+                      htmlFor="address"
+                    >
+                      Address
+                    </label>
+                    <input
+                      type="text"
+                      id="address"
+                      value={formData.address}
+                      onChange={handleChange}
+                      className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.address ? "border-red-500" : ""}`}
+                    />
+                    {errors.address && (
+                      <p className="text-xs italic text-red-500">
+                        Please enter the address.
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="mb-2 block font-bold text-gray-700"
+                      htmlFor="mobile-number"
+                    >
+                      Mobile Number
+                    </label>
+                    <input
+                      type="text"
+                      id="mobileNumber"
+                      value={formData.mobileNumber}
+                      onChange={handleChange}
+                      className={`focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none ${errors.mobileNumber ? "border-red-500" : ""}`}
+                      placeholder="+91xxxxxxxxxx"
+                    />
+                    {errors.mobileNumber && (
+                      <p className="text-xs italic text-red-500">
+                        Please enter a valid mobile number.
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-4">
+                    <label className="mb-2 block font-bold text-gray-700">
+                      Interested in Demo Class
+                    </label>
+                    <div className="flex items-center">
+                      <input
+                        type="radio"
+                        id="yes"
+                        name="demoClass"
+                        value="yes"
+                        checked={formData.demoClass === "yes"}
+                        onChange={handleRadioChange}
+                        className="mr-2 leading-tight"
+                      />
+                      <label className="text-gray-700" htmlFor="yes">
+                        Yes
+                      </label>
+                      <input
+                        type="radio"
+                        id="no"
+                        name="demoClass"
+                        value="no"
+                        checked={formData.demoClass === "no"}
+                        onChange={handleRadioChange}
+                        className="ml-4 mr-2 leading-tight"
+                      />
+                      <label className="text-gray-700" htmlFor="no">
+                        No
+                      </label>
+                    </div>
+                    {errors.demoClass && (
+                      <p className="text-xs italic text-red-500">
+                        Please select an option.
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-4 flex h-full items-center">
+                    <button
+                      type="submit"
+                      className="focus:shadow-outline bg-bgcust rounded px-8 py-2 uppercase text-white focus:outline-none"
+                    >
+                      Continue
+                    </button>
+                    <label className="mx-8 mb-2 flex h-full items-center text-gray-700">
+                      <input
+                        type="checkbox"
+                        id="remember-me"
+                        className="mr-2 leading-tight"
+                      />
+                      Remember me!
                     </label>
                   </div>
-                  {errors.demoClass && (
-                    <p className="text-xs italic text-red-500">
-                      Please select an option.
-                    </p>
-                  )}
+                </form>
+              ) : (
+                <div className="grid h-full min-h-[75vh] place-content-center space-y-2">
+                  <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full border bg-[#007aff26]">
+                    <Check className="text-bluemain h-12 w-12" />
+                  </div>
+                  <div className="text-bluemain text-center">
+                    <h1 className="text-lg">Thank you!</h1>
+                    <h2 className="poppins-semibold text-2xl tracking-wide ">
+                      We will reach out to
+                      <br /> you soon.
+                    </h2>
+                  </div>
                 </div>
-                <div className="mb-4 flex h-full items-center">
-                  <button
-                    type="submit"
-                    className="focus:shadow-outline bg-bgcust rounded px-4 py-2 font-bold text-white focus:outline-none"
-                  >
-                    Continue
-                  </button>
-                  <label className="mx-8 mb-2 block font-bold text-gray-700">
-                    <input
-                      type="checkbox"
-                      id="remember-me"
-                      className="mr-2 leading-tight"
-                    />
-                    Remember me!
-                  </label>
-                </div>
-              </form>
+              )}
             </div>
           </div>
         </div>
